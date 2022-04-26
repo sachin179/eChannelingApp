d8e9ad10e79666576e862bbdff00fb3419e65cac
sachinharshitha179@gmail.com <sachinharshitha179@gmail.com>
2022-04-26 17:50:17 +0530

    update

--- app/src/main/java/com/example/crud/MainActivity.java
+++ app/src/main/java/com/example/crud/MainActivity.java
@@ -2,19 +2,54 @@ package com.example.crud;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView RegisterLink_2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText userName = findViewById(R.id.userName);
+        final EditText rlPassword = findViewById(R.id.rlPassword);
+        final Button lbutton = findViewById(R.id.lbutton);
+        final TextView RegisterLinktxt = findViewById(R.id.RegisterLink_2);
+
+        lbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                final String userNametxt = userName.getText().toString();
+                final String passwordtext = rlPassword.getText().toString();
+
+                if (userNametxt.isEmpty() || passwordtext.isEmpty()){
+                    Toast.makeText(MainActivity.this, "pleace enter your mobile or password", Toast.LENGTH_SHORT).show();
+                }else {
+
+                }
+
+            }
+
+        });
+
+        RegisterLinktxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, MainActivity2.class));
+            }
+        });
+
         // final EditText edit_name = findViewById(R.id.edit_name);
         //  final EditText edit_position = findViewById(R.id.edit_position);
         //   Button btn = findViewById(R.id.btn_submit);
--- app/src/main/java/com/example/crud/MainActivity2.java
+++ app/src/main/java/com/example/crud/MainActivity2.java
@@ -4,17 +4,64 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity2 extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity2 extends AppCompatActivity implements AdapterView.OnItemSelectedListener, View.OnClickListener {
+
+    private FirebaseAuth mAuth;
+    private EditText rFullName,rPassword,editTextTextPassword2,rollSpin,editTextPhone;
+    private TextView button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar();
+
+        final EditText rFullName = findViewById(R.id.rFullName);
+        final EditText rPassword = findViewById(R.id.rPassword);
+        final EditText cPassword = findViewById(R.id.cPassword);
+        final Spinner rollSpin = findViewById(R.id.rollSpin);
+        final EditText rphonenu = findViewById(R.id.rphonenu);
+
+        final Button rbutton1 = findViewById(R.id.rbutton1);
+
+        rbutton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //get data from eidttext into string variable
+                final String rFullNametxt = rFullName.getText().toString();
+                final String rPasswordtxt = rPassword.getText().toString();
+                final String cPasswordtxt = cPassword.getText().toString();
+                final String rollSpintxt = rphonenu.getText().toString();
+                final String rphonenutxt = rphonenu.getText().toString();
+
+                //check if user fill all the field befor sending data to firebase
+                if(rFullNametxt.isEmpty() || rPasswordtxt.isEmpty() || cPasswordtxt.isEmpty() || rollSpintxt.isEmpty() || rphonenutxt.isEmpty()){
+
+                    Toast.makeText(MainActivity2.this,"pleace all the field")
+                }
+            }
+        });
+
+
         setContentView(R.layout.activity_main2);
+        mAuth = FirebaseAuth.getInstance();
+
+
+
+
 
         Spinner spinner = findViewById(R.id.rollSpin);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.position, android.R.layout.simple_spinner_item);
@@ -22,6 +69,7 @@ public class MainActivity2 extends AppCompatActivity implements AdapterView.OnIt
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
     }
+
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -31,6 +79,11 @@ public class MainActivity2 extends AppCompatActivity implements AdapterView.OnIt
 
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+
+    @Override
+    public void onClick(View view) {
 
     }
 }
