6ca7c376b897a5a2e75b1d1705568b57cda5540a
sachinharshitha179@gmail.com <sachinharshitha179@gmail.com>
2022-04-29 11:34:17 +0530

    mri insert data

--- app/src/main/java/com/example/crud/mriAppoinment.java
+++ app/src/main/java/com/example/crud/mriAppoinment.java
@@ -11,6 +11,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
@@ -18,6 +20,11 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
@@ -25,17 +32,29 @@ import com.google.firebase.storage.UploadTask;
 
 import java.util.UUID;
 
-public class mriAppoinment extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class mriAppoinment extends AppCompatActivity implements AdapterView.OnItemSelectedListener ,View.OnClickListener {
 
   private ImageView profilepc;
   public Uri imageUri;
   private FirebaseStorage storage;
   private StorageReference storageReference;
 
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://echannelling-b9e97-default-rtdb.firebaseio.com/");
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mri_appoinment);
+
+        // final Spinner Booktype = findViewById(R.id.textView3);
+        final EditText pName = findViewById(R.id.editTextTextPersonName);
+        final EditText mriPhonenu = findViewById(R.id.mriPNu);
+        final EditText moreDetail = findViewById(R.id.et_input);
+
+        final Button saveBtn = findViewById(R.id.bt_save);
+
+
+
 
         profilepc = findViewById(R.id.profilepc);
         storage = FirebaseStorage.getInstance();
@@ -54,6 +73,71 @@ public class mriAppoinment extends AppCompatActivity implements AdapterView.OnIt
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
 
+        //////////////////////////////////////////
+
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //get data from eidttext into string variable
+                // final String Booktypetxt = Booktype.getText().toString();
+                final String pNametxt = pName.getText().toString();
+                final String mriPhonenutxt = mriPhonenu.getText().toString();
+                final String moreDetailtxt = moreDetail.getText().toString();
+
+
+                //check if user fill all the field befor sending data to firebase
+                if(pNametxt.isEmpty() || mriPhonenutxt.isEmpty() || moreDetailtxt.isEmpty()){
+
+                    Toast.makeText(mriAppoinment.this,"pleace all the field",Toast.LENGTH_SHORT).show();
+                }
+                //check if password are matching with each other
+                //if not matching with each other then show a tost messsage
+
+
+                else{
+                    databaseReference.child("mridata").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            //check is phone is phone number is not registered before
+
+                            if(snapshot.hasChild(mriPhonenutxt)){
+                                Toast.makeText(mriAppoinment.this, "mri already exist", Toast.LENGTH_SHORT).show();
+                            }else {
+                                // databaseReference.child("users").addListenerForSingleValueEvent(new);
+                                //sendinh data to firebase realtimr database
+                                //we are using phone number as unique identity of every user
+                                //so all the other details of user comes under the phone number
+                                databaseReference.child("mridata").child(mriPhonenutxt).child("pName").setValue(pNametxt);
+                                databaseReference.child("mridata").child(mriPhonenutxt).child("moreDetail").setValue(moreDetailtxt);
+
+                                //show a success then finish the activity
+                                Toast.makeText(mriAppoinment.this, "user register successfully", Toast.LENGTH_SHORT).show();
+                                finish();
+
+                                saveBtn.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View view) {
+                                        startActivity(new Intent(mriAppoinment.this, Home.class));
+                                    }
+                                });
+
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+
+
+
+                }
+            }
+        });
+
+
+        /////////////////////////////////////////
 
     }
 
@@ -116,6 +200,11 @@ public class mriAppoinment extends AppCompatActivity implements AdapterView.OnIt
 
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+
+    @Override
+    public void onClick(View view) {
 
     }
 }
